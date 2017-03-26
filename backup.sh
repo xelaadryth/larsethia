7135612a1ecb1f73370c63fcbf2a0668841a1505
Xelaadryth <xdelaadryth@github.com>
2017-03-26 14:41:39 -0700

    Removing git pushes and just making local backups

--- backup.sh
+++ backup.sh
@@ -3,11 +3,14 @@ git_access_token=`cat git_access_token.txt`
 git_user=xelaadryth
 git_repo=larsethia
 git_branch=origin/master
+target_db=server/evennia.db3
+backup_dir=server/backups
 
 # Set up Git
+echo Setting Git configurations.
 git config --global user.name "auto_puller"
 git remote set-url origin https://${git_user}:${git_access_token}@github.com/${git_user}/${git_repo}.git
-# Let the remote get added
+# Pull branch info
 git fetch
 git branch --set-upstream-to=${git_branch}
 
@@ -15,19 +18,15 @@ last_datestamp=`date +"%Y%m%d%H%M%S"`
 
 while true
 do
+    echo Starting polling.
     datestamp=`date +"%Y%m%d%H%M%S"`
 
-    cd ${update_dir}
     git reset HEAD --hard
     # If the GitHub repo changed at all or a day has passed, backup the db
     if test `git pull -f | wc -l` -gt 1 || test ${datestamp} -gt `expr ${last_datestamp} + 1000000`
     then
         echo Backing up database at ${datestamp}.
-        sqlite3 ${target_file} ".backup ${backup_directory}/${datestamp}-evennia.db3.bak"
-        cd ${backup_dir}
-        git add .
-        git commit -m "Backing up database at ${datestamp}."
-        git push origin master -f
+        sqlite3 ${target_db} ".backup ${backup_dir}/${datestamp}-evennia.db3.bak"
         last_datestamp=${datestamp}
     else
         echo No backup necessary.
