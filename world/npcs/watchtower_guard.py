ec2c692da6bf71f4e3debc0dd5e97b5299bdabb9
TheHonorableKall <cjbonifati@gmail.com>
2017-04-06 13:36:28 -0400

    dialogue for guard Meruvia's Temple
    still trying

---
+++ world/npcs/watchtower_guard.py
@@ -0,0 +1,44 @@
+def start(caller):
+    text = "Aye! Who goes there!"
+
+    options = [
+        {
+            "dsec": '"A Traveler"',
+            "goto": "traveler"
+        },
+        {
+            "dsec": '"A Man of Faith"',
+            "goto": "faith"
+        },
+        {
+            "desc": '"A Warrior'",
+            "goto": "warrior"
+        }
+    ]
+    return text, options
+
+def traveler(caller):
+    text = "Ah, a nomad. There is food and lodging inside the temple, if you have the coin."
+    options = [
+        {
+            "desc": '"Very well..."',
+            "goto": "end"
+        }
+    ]
+    return text, options
+    
+def faith(caller):
+    text = "Another pilgrim who wishes to be graced by Meruvia. Enter, child"
+    options = [
+        {
+            "desc": '"My thanks"',
+            "goto": "end"
+        }
+    ]
+    return text, options
+    
+def warrior(caller):
+    return '"Aye, we/'ll see anout that, Meruvia will bless those who battle the darkness. Go forth, and may Meruvia\'s light bless you."', []
+    
+def end(caller):
+    return '"Go forth, and may Meruvia\'s light bless you."', []
